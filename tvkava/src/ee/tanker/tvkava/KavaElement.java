886b2887755a7f30ba07ec95038388a0f5998b75
Tanel Kaare <tanel.kaare@gmail.com>
2015-09-15 23:25:54 +0300

    commit

---
+++ tvkava/src/ee/tanker/tvkava/KavaElement.java
@@ -0,0 +1,13 @@
+package ee.tanker.tvkava;
+
+public class KavaElement {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ tvkava/src/ee/tanker/tvkava/KavaReader.java
@@ -0,0 +1,13 @@
+package ee.tanker.tvkava;
+
+public class KavaReader {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
